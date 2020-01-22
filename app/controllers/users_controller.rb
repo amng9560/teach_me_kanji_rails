b1ad69d51c531a1eb32fa248b932e3832af9f456
Amy Nguyen <amy.nguyen730@gmail.com>
2020-01-22 16:24:01 -0700

    changes errors, adds statuses, adds validations in users model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
             @user.save
             render json: @user
         else
-            render json: { error: @user.errors.full_messages }
+            render json: { error: @user.errors.full_messages }, status: 409
         end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
     has_many :user_words
     has_many :words, through: :user_words
 
+    validates :first_name, presence: true
+    validates :last_name, presence: true
     validates :username, presence: true, uniqueness: true
-    validates :password, presence: true
+    validates :password, presence: true, length: { minimum: 3 }
 end
